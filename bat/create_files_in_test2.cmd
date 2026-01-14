d6765910019f54e3e7cb4d5eeeb7b9b50e2ecfd2
John Zoetebier <john@zoetebier.net>
2026-01-14 14:44:19 +1300

    Stop tracking some files and folders

--- bat/create_files_in_test2.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-@REM echo "This is a test to trigger a monitor" > D:\Development\FolderMonitor\data\Source\Cloud\Test2/test1.txt
-@REM echo "This is a test to trigger a monitor" > D:\Development\FolderMonitor\data\Source\Cloud\Test2/test2.txt
-@REM mkdir -p "D:\Development\FolderMonitor\data\Source\Cloud\Test2/Subfolder"
-@REM echo "This is a test to trigger a monitor" > D:\Development\FolderMonitor\data\Source\Cloud\Test2/Subfolder/test1.txt
-@REM echo "This is a test to trigger a monitor" > D:\Development\FolderMonitor\data\Source\Cloud\Test2/Subfolder/test2.txt
-
-cp -r .\data\Source\Cloud\Test2\ .\data\Destination\Cloud\Test2
--- bin/run_folder_monitor.sh
+++
@@ -1,4 +0,0 @@
-!#/usr/bin/bash
-
-#  Run this script in main folder
-python ../scripts/folder_monitor.py --config-path "conf/config.yaml"
